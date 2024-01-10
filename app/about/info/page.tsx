9a43a07f5106f53a5836d9d48d3517e14dd399ec
Marco Castanho <marco.castanho@toptal.com>
2024-01-10 20:24:38 +0000

    nested routes

---
+++ app/about/info/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InfoPage = () => {
+    return (
+        <h1 className='text-7xl'>
+            About Info Page
+        </h1>
+    )
+}
+
+export default InfoPage
