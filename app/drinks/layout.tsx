360f12941ca2950cb7330272798b61afa2e60fc3
Marco Castanho <marco.castanho@toptal.com>
2024-02-07 20:34:21 +0000

    21. nested layouts

---
+++ app/drinks/layout.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+const DrinksLayout: FC<Props> = ({ children }) => {
+  return (
+    <div className="max-w-xl">
+      <div className="mockup-code mb-8">
+        <pre data-prefix="$">
+          <code>npx create-next-app@latest nextjs-tutorial</code>
+        </pre>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default DrinksLayout;
