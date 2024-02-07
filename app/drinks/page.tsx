cf74843e4a25a9cba647daf5f60c7dd8bbd7cb00
Marco Castanho <marco.castanho@toptal.com>
2024-02-07 19:59:29 +0000

    18. fetch data

--- app/drinks/page.tsx
+++ app/drinks/page.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const DrinksPage = () => {
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+
+const DrinksPage = async () => {
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data);
+
+
   return (
     <h1 className='text-7xl'>
       Drinks Page
