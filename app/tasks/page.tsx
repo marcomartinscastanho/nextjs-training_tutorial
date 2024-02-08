9ff0eeae78830b0d640558cb936ae693f6e81541
Marco Castanho <marco.castanho@toptal.com>
2024-02-08 20:09:45 +0000

    51

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -2,6 +2,8 @@ import TaskFormV2 from "@/components/TaskFormV2";
 import TaskList from "@/components/TaskList";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const TasksPage = () => {
   return (
     <div className="max-w-lg">
