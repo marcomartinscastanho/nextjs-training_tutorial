2eb427115af93f2ed44df1ff021f049ae9b94678
Marco Castanho <marco.castanho@toptal.com>
2024-02-08 16:02:06 +0000

    43. delete button

--- components/DeleteForm.tsx
+++ components/DeleteForm.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import { deleteTask } from "@/utils/actions";
 import React, { FC } from "react";
+import { useFormStatus } from "react-dom";
 
 interface Props {
   id: string;
 }
 
+const DeleteButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button className="btn btn-xs btn-error" disabled={pending}>
+      {pending ? "deleting..." : "DELETE"}
+    </button>
+  );
+};
+
 const DeleteForm: FC<Props> = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-xs btn-error">DELETE</button>
+      <DeleteButton />
     </form>
   );
 };
