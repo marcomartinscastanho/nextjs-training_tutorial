453ed2e8f2392b2eb57708a28e30e590ea322b0e
Marco Castanho <marco.castanho@toptal.com>
2024-02-08 14:18:52 +0000

    34. first server action

--- components/TaskForm.tsx
+++ components/TaskForm.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
+
+const createTask = async (formData: FormData) => {
+  "use server";
+
+  const content = formData.get("content") as string;
+  if (content) {
+    await prisma.task.create({ data: { content } });
+    revalidatePath("/tasks");
+  }
+};
 
 const TaskForm = () => {
-  return <div>TaskForm</div>;
+  return (
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          type="text"
+          className="input input-bordered join-item w-full"
+          placeholder="type here"
+          name="content"
+          required
+        />
+        <button type="submit" className="btn btn-primary join-item">
+          CREATE TASK
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default TaskForm;
